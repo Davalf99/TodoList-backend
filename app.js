773a19fb4b77fb5099acf62ef298ccf02eed79a1
David Alfonso <davalf99@gmail.com>
2021-10-27 13:37:15 -0600

    Backend setup with cors

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+var cors = require('cors')
+
+app.use(cors()) // Use this after the variable declaration
 
 app.get('/',(req, res)=>{
-    res.send('Hello theewere');
+    res.send('Hello there');
 })
 
 app.listen(8080, ()=>{
