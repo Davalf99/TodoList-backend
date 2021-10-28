8c0a8680932c2cd95546617a4fe7111b71a37953
David Alfonso <davalf99@gmail.com>
2021-10-28 13:06:55 -0600

    Can receive request from front and update list

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
-var cors = require('cors')
+var cors = require('cors');
+var list = ['bob', 'ted', 'rob'];
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 app.use(cors()) // Use this after the variable declaration
 
 app.get('/',(req, res)=>{
-    res.send('Hello there');
-})
+    res.send(list);
+});
+
+app.post('/',(req,res)=>{
+    console.log(req.body);
+    list.push(req.body.item);
+    res.send(req.body.item);
+});
 
 app.listen(8080, ()=>{
     console.log('Listening on port 8080');
